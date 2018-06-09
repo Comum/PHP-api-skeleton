c1b0c326621d12a6f6648228dd496893d3e064ff
Comum <miguel.rib.20@gmail.com>
2018-06-09 17:20:57 +0100

    Simplify RouteNotFound controller

--- app/controllers/routeNotFound.php
+++ app/controllers/routeNotFound.php
@@ -1,14 +1,10 @@
 <?php
     class RouteNotFound extends Controller {
         public function index() {
-            $response = $this->generateResponse();
+            $response = $this->getErrorResponse();
             $post_data = json_encode($response, JSON_FORCE_OBJECT);
 
             echo $post_data;
-        }
-
-        private function generateResponse() {
-            return $this->getErrorResponse();
         }
     }
 ?>
