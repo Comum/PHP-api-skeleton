04c699e723460aa225a3d569c0eed1a510abcdda
Comum <miguel.rib.20@gmail.com>
2018-06-09 15:39:04 +0100

    Setting up routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,17 @@
 <?php
     class App {
+        protected $controller = 'home';
+        protected $method = 'index';
+        protected $params = [];
+
         public function __construct() {
-            
+            print_r($this->parseUrl());
+        }
+
+        public function parseUrl() {
+            if (isset($_GET['url'])) {
+                return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+            }
         }
     }
 ?>
